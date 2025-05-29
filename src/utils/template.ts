daf784dabd2a84a1bc82ed2683666104298e550a
francisco.diaz <francisco.diaz@un.org>
2025-05-29 18:38:24 +0200

    fix: template handling and build process

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 import * as Handlebars from 'handlebars'
 
 export function renderTemplate(templateName: string, context: object = {}) {
-  const template = readFileSync(join(__dirname, '..', '..', 'views', `${templateName}.hbs`), 'utf-8')
+  const template = readFileSync(join(process.cwd(), 'dist', 'views', `${templateName}.hbs`), 'utf-8')
   const compiledTemplate = Handlebars.compile(template)
   return compiledTemplate(context)
 } 
